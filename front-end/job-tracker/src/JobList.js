16071f5c2ede775180aefecf9fc4172f8aa45351
Mohammad5566 <mohammadmahmud5566@yahoo.com>
2025-03-31 20:17:31 -0400

    added front end folder, set up basic react front end

---
+++ front-end/job-tracker/src/JobList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const JobList = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/jobs").then((res) => {
+      setJobs(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Job Applications</h1>
+      <ul>
+        {jobs.map((job) => (
+          <li key={job.id}>{job.company} - {job.position} ({job.status})</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default JobList;
